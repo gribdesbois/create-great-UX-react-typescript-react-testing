9e8699c45696fa1ec170de2dfb5e6da8a3e1d0c9
gribdesbois <gribdesbois@gmail.com>
2022-03-07 14:56:22 +0100

    search

---
+++ vite-project/src/components/Menu/MenuItems.tsx
@@ -0,0 +1,15 @@
+import Menu from '.'
+import { MenuItem } from '../../types/MenuItem'
+
+const menuItems: MenuItem[] = [
+  {
+    linkTo: '/',
+    label: 'Home',
+    key: 'link-to-home',
+  },
+  {
+    linkTo: '/search',
+    label: 'Search',
+    key: 'link-to-search',
+  },
+]
--- vite-project/src/components/Search/index.tsx
+++ vite-project/src/components/Search/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getProducts } from '../../api'
 
 const Search = () => {
-  return <h1>It's the search!</h1>
+  const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [hasError, setHasError] = useState<boolean>(false)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setIsLoading(false)
+        const fetchedProducts = await getProducts()
+        setIsLoading(false)
+        setProducts(fetchedProducts)
+      } catch (err) {
+        setIsLoading(false)
+        setHasError(true)
+      }
+    }
+    fetchProducts()
+  }, [])
 }
 
 export default Search
