3f4890c9e874b34232ea5544c2f44dcfac2a3450
gribdesbois <gribdesbois@gmail.com>
2022-03-07 15:25:02 +0100

    confused tutorial with no provided code. probably won't ever be able to finish it. we'll see

---
+++ vite-project/src/reducers/fetchReducer.tsx/index.tsx
@@ -0,0 +1,26 @@
+import { State, FetchActionType, FetchAction } from './../../types'
+
+export const fetchReducer = (state: State, action: FetchAction): State => {
+  switch (action.type) {
+    case FetchActionType.FETCH_INIT:
+      return {
+        ...state,
+        isLoading: true,
+        hasError: false,
+      }
+    case FetchActionType.FETCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        hasError: false,
+      }
+    case FetchActionType.FETCH_ERROR:
+      return {
+        ...state,
+        hasError: true,
+        isLoading: false,
+      }
+    default:
+      return state
+  }
+}
